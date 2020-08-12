113254e13ea3f3410b01d20dae2a9a9e6563ba2a
Ola <olawunmi263@yahoo.com>
2020-08-12 02:11:34 +0100

    More For Loop

---
+++ src/chapter1/CompoundInterestForLoop.java
@@ -0,0 +1,17 @@
+package chapter1;
+//Calc number of subscribers in 20 days if no of subs is currently 10k
+//Compound interest formula is A= P(1+R)^n
+
+public class CompoundInterestForLoop {
+    public static void main(String[] args) {
+//        double amount; //no of subs
+//        double principal = 10000; // no of current subs ie value we're starting with
+//        double rate = 0.01; //rate of increase in subs per day
+//
+//        for (int day = 0; day <=20; day++){
+//            amount = principal*Math.pow(1+rate,day);
+//            System.out.println(day + " "+ amount);
+//        }
+        System.out.println(Math.ceil(4.177));
+    }
+}

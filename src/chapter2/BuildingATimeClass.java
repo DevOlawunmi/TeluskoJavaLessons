c0f7b7eea8969e75d3ba15533737b7892727e4f6
Ola <olawunmi263@yahoo.com>
2020-08-15 04:01:26 +0100

    Time Practise

---
+++ src/chapter2/BuildingATimeClass.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class BuildingATimeClass {
+    public static void main(String[] args) {
+        Time timeObject = new Time();
+
+        timeObject.setTime(18,22,17);
+        System.out.println(timeObject.toString());
+
+    }
+}

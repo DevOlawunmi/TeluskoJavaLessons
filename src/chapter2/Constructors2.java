81f5ffa85e4f408da219b5fc5c9ead02f896b83c
Ola <olawunmi263@yahoo.com>
2020-08-20 01:52:38 +0100

    Constructors Car update

--- src/chapter2/Constructors2.java
+++ src/chapter2/Constructors2.java
@@ -2,17 +2,17 @@ package chapter2;
 
 public class Constructors2 {
 
-    private int x; //class variable
+    private int name; //class variable
 
 
     public Constructors2 (int y){
-        this.x=y;
+        this.name=y;//if we call this constructor, assign the value y to the variable 'name'
     }
 
 
 public static void main(String[] args){
      Constructors2 myObj = new Constructors2(5);
-    System.out.println(myObj.x);
+    System.out.println(myObj.name);
 }
 
 

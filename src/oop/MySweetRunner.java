de0ce2a1c0fea3f8fc445a8952890556693291e0
Ola <olawunmi263@yahoo.com>
2020-08-20 02:22:34 +0100

    Classes and Objects

---
+++ src/oop/MySweetRunner.java
@@ -0,0 +1,11 @@
+package oop;
+//fields are class level variables
+
+public class MySweetRunner {
+    public static void main(String[] args) {
+User user1 = new User();
+user1.firstName = "Dolly";
+user1.lastName = "Parton";
+user1.output(); // calling the output method on the user1 object
+    }
+}
---
+++ src/oop/User.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class User {
+    //these are variables available to the User class
+    public String firstName;
+    public String lastName;
+
+    //we can also create a method for the class
+    public void output(){
+        System.out.println(firstName +" " +lastName);
+    }
+}

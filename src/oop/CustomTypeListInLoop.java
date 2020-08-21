487265c4a4b71ebdce02af3e29a3f0760aa690bd
Ola <olawunmi263@yahoo.com>
2020-08-21 04:56:23 +0100

    Custom type list in a loop2

--- src/oop/CustomTypeListInLoop.java
+++ src/oop/CustomTypeListInLoop.java
@@ -9,16 +9,23 @@ public class CustomTypeListInLoop {
         String[] lastNames  = { "Okereke", "Orji", "Adegoke", "Adewusi"};
         List<User> myYNames = new ArrayList<>();
         for (int i = 0; i < firstNames.length; i++) {
-            User u = new User();
-            u.setFirstName(firstNames[i]);// set the current iteration of i as first name
-            u.setLastName(lastNames[i]);// set the current iteration of i as last name
-            myYNames.add(u);
+            User u = new User(); //create a new user
+            u.setFirstName(firstNames[i]);// set the current iteration of i as u's first name
+            u.setLastName(lastNames[i]);// set the current iteration of i as u's last name
+            myYNames.add(u); // add u to the list called myYNames
         }
         for (User u:myYNames
              ) {
             System.out.println(u.getFullName());
 
         }
+        CustomTypeListInLoop ctl = new CustomTypeListInLoop();
+        ctl.printUser(myYNames.get(1));
+    }
+
+    // to create a method that takes custom list as argument
+    public void printUser(User u){
+        System.out.println("This is from the print user method: " + u.getFullName());
     }
 
 }
